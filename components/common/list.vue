165b2797ff0c78df1afe2fbe8af59a5764449271
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2022-01-09 19:49:52 +0900

    initial commit

---
+++ components/common/list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <template v-if='items.length'>
+      <div v-for="(item, index) in items" :key="index">
+        <h3>{{item.name}}</h3>
+        <ul>
+          <li>{{item.position}}</li>
+          <li>{{item.team}}</li>
+        </ul>
+      </div>
+    </template>
+
+    <template v-else>
+      <p>表示するデータがありません</p>
+    </template>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'List',
+  data() {
+    return {
+      sample: 'ぐらふきゅえる'
+    }
+  },
+  props: {
+    items: {
+      type: Array,
+      default: [],
+      required: true
+    },
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <h1>選手一覧⚽️</h1>
+  <List :items="items"></List>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import List from '@/components/common/list.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    List
+  },
+  data() {
+    return {
+      items: [
+        {
+          position: 'FW',
+          name: 'メッシ',
+          team: 'パリサンジェルマン',
+        },
+        {
+          position: 'FW',
+          name: 'クリスティアーノ・ロナウド',
+          team: 'マンチェスターユナイテッド',
+        },
+        {
+          position: 'FW',
+          name: 'トーマス・三浦',
+          team: 'バイオリンミュンヘン',
+        }
+      ]
+    }
+  }
+})
+</script>
